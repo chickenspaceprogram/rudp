4f9f75f2d03160edf00fb7ff3552f712d7ffee0d
Athena Boose <pestpestthechicken@yahoo.com>
2025-05-24 21:03:06 -0700

    more timer stuff

--- src/polling/timers.h
+++ src/polling/timers.h
@@ -2,20 +2,25 @@
 #include <stdint.h>
 #include "minheap.h"
 #include <rudp/poll.h>
+#include <time.h>
 
 struct rudp_timer_internal {
-	uint64_t timeout;
-	uint64_t nrepeats;
+	int64_t timeout;
 	struct rudp_data data;
 	int64_t id;
+	uint64_t nrepeats;
 };
 
 struct rudp_timerheap {
 	struct rudp_min_heap *heap;
 };
 
-struct rudp_min_heap *rudp_make_timerheap(void);
-int rudp_timerheap_add(struct rudp_min_heap *timerheap, const struct rudp_timer *tmr);
+int rudp_timerheap_new(struct rudp_timerheap *timerheap);
 
-int rudp_timerheap_del
+int rudp_timerheap_add(struct rudp_timerheap *timerheap, const struct timespec *cur_time, const struct rudp_timer_internal *tmr);
+int rudp_timerheap_del(struct rudp_timerheap *timerheap, int64_t id);
+
+int64_t rudp_timerheap_get_wait(struct rudp_timerheap *timerheap, const struct timespec *cur_time);
+int64_t rudp_timerheap_get_expired(struct rudp_timerheap *timerheap, const struct timespec *cur_time, struct rudp_timer_internal *out, size_t nout);
+
 
