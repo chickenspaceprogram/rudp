5a036117d354f62a851f581c2e9d2942bcc844a3
Athena Boose <pestpestthechicken@yahoo.com>
2025-06-08 17:28:37 -0700

    Made vector take values instead of pointers.

--- src/utils/include/rudp/utils/vector.h
+++ src/utils/include/rudp/utils/vector.h
@@ -3,63 +3,100 @@
 #include <stddef.h>
 #include <rudp/utils/intmanip.h>
 
+// Creating any variable whose name starts with RUDP_VECTOR and ends with _INTERNAL_ is disallowed
+
 #ifndef RUDP_VECTOR_INIT_SIZE
 #define RUDP_VECTOR_INIT_SIZE 8
 #endif
 
+// Expands to the type of a rudp_vector.
+// 
+// ELEMENTS_TYPE is the type of the vector elements. It should not be const-qualified.
 #define RUDP_VECTOR_TYPE(ELEMENTS_TYPE)\
-	struct {\
-		ELEMENTS_TYPE *data;\
-		size_t nel;\
-		size_t bufsize;\
-	}
+struct {\
+	ELEMENTS_TYPE *data;\
+	size_t nel;\
+	size_t bufsize;\
+}
 
-#define rudp_vector_new(VECPTR, ALLOCATOR_PTR)\
-	((VECPTR)->data = rudp_allocator_alloc(sizeof(*(VECPTR)->data) * (RUDP_VECTOR_INIT_SIZE), (ALLOCATOR_PTR)),\
-	(VECPTR)->nel = 0,\
-	(VECPTR)->bufsize = (RUDP_VECTOR_INIT_SIZE),\
-	((VECPTR)->data == NULL) ? -1 : 0)\
+// Creates a new rudp_vector, given the name of a rudp_vector struct.
+// VEC must be a rudp_vector struct that has a type given by RUDP_VECTOR_TYPE() for some type.
+// ALLOCATOR_PTR is a pointer to a rudp_allocator struct. It may be NULL.
+#define rudp_vector_new(VEC, ALLOCATOR_PTR)\
+	((VEC).data = rudp_allocator_alloc(sizeof(*(VEC).data) * (RUDP_VECTOR_INIT_SIZE), (ALLOCATOR_PTR)),\
+	(VEC).nel = 0,\
+	(VEC).bufsize = (RUDP_VECTOR_INIT_SIZE),\
+	((VEC).data == NULL) ? -1 : 0)\
 
-
-#define rudp_vector_delete(VECPTR, ALLOCATOR_PTR)\
-	(rudp_allocator_free((VECPTR)->data, (VECPTR)->bufsize, (ALLOCATOR_PTR)))
+// Deletes a rudp_vector.
+// VEC must be the name of a rudp_vector previously initialized by rudp_vector_new.
+// ALLOCATOR_PTR is a pointer to a rudp_allocator struct. It may be NULL.
+// ALLOCATOR_PTR must be the same allocator previously used on the vector.
+#define rudp_vector_delete(VEC, ALLOCATOR_PTR)\
+	(rudp_allocator_free((VEC).data, (VEC).bufsize, (ALLOCATOR_PTR)))
 	
+// Gets the number of elements in a rudp_vector.
+// VEC must be the name of a rudp_vector previously initialized by rudp_vector_new.
+#define rudp_vector_size(VEC)	((VEC).nel)
 
-#define rudp_vector_size(VECPTR)	((VECPTR)->nel)
-#define rudp_vector_capacity(VECPTR)	((VECPTR)->bufsize)
-#define rudp_vector_erase(VECPTR)	(VECPTR)->nel = 0;
-#define rudp_vector_empty(VECPTR)	((VECPTR)->nel == 0)
+// Gets the capacity of a rudp_vector; in other words, the number of elements it can hold without reallocating.
+// VEC must be the name of a rudp_vector previously initialized by rudp_vector_new.
+#define rudp_vector_capacity(VEC)	((VEC).bufsize)
 
+// Deletes all elements from a rudp_vector. Returns nothing.
+// VEC must be the name of a rudp_vector previously initialized by rudp_vector_new.
+#define rudp_vector_erase(VEC)	(VEC).nel = 0;
 
-#define rudp_vector_at(VECPTR, INDEX) (((VECPTR)->data) + (INDEX))
+// Returns 1 if the rudp_vector is empty, 0 if not.
+// VEC must be the name of a rudp_vector previously initialized by rudp_vector_new.
+#define rudp_vector_empty(VEC)	((VEC).nel == 0)
 
-#define rudp_vector_push(VECPTR, ELEMPTR, ALLOC)\
-_Generic((ELEMPTR), typeof((VECPTR)->data):\
-	((rudp_vector_reserve((VECPTR), (VECPTR)->nel + 1, (ALLOC)) == 0) ? (\
-	(VECPTR)->data[((VECPTR)->nel)++] = *(ELEMPTR),0\
+// Expands to the value of the element of VEC at INDEX.
+// Effectively, this is VEC[INDEX] if VEC was a normal array.
+// INDEX must be an integer type, and it must be greater than 0 and less than rudp_vector_capacity(VEC)
+#define rudp_vector_at(VEC, INDEX) (((VEC).data) + (INDEX))
+
+// Appends the value of the variable named by ELEM to the end of VEC.
+// VEC must be the name of a rudp_vector previously initialized by rudp_vector_new.
+// ELEM must be the expression whose value you wish to append to VEC.
+// ALLOC must be the same allocator previously used with the vector.
+#define rudp_vector_push(VEC, ELEM, ALLOC)\
+_Generic((ELEM), typeof(*(VEC).data):\
+	((rudp_vector_reserve((VEC), (VEC).nel + 1, (ALLOC)) == 0) ? (\
+	(VEC).data[((VEC).nel)++] = (ELEM),0\
 	) : -1)\
 )
 
-#define rudp_vector_pop(VECPTR)\
-	(VECPTR)->nel = ((VECPTR)->nel == 0 ? 0 : (VECPTR)->nel - 1);\
+// Pops the last element from VEC.
+// This function is safe in that popping from an empty vector does nothing.
+#define rudp_vector_pop(VEC)\
+	(VEC).nel = ((VEC).nel == 0 ? 0 : (VEC).nel - 1);\
 
-// returns 0 if successful, -1 if an error occurs.
-// you'll still have to free the vector if an error happens
-#define rudp_vector_reserve(VECPTR, NEWSIZE, ALLOC)\
-	rudp_vector_reserve_exact((VECPTR), rudp_next_pwr_2((NEWSIZE)), (ALLOC))
+// Reserves space for at least NEWSIZE elements in VEC.
+// VEC must be the name of a rudp_vector previously initialized by rudp_vector_new.
+// NEWSIZE must be an integer type.
+// ALLOC must be the same allocator previously used with the vector.
+//
+// Space for more than NEWSIZE elements may be allocated.
+// Returns 0 if successful, -1 if an error occurs.
+// If an error occurs, the vector is unchanged.
+#define rudp_vector_reserve(VEC, NEWSIZE, ALLOC)\
+	rudp_vector_reserve_exact((VEC), rudp_next_pwr_2((NEWSIZE)), (ALLOC))
 
-#define rudp_vector_reserve_exact(VECPTR, NEWSIZE, ALLOC) ({\
-	int retval = 0;\
-	if ((NEWSIZE) > (VECPTR)->bufsize) {\
-		typeof((VECPTR)->data) newptr = rudp_allocator_reallocarray((VECPTR)->data, (NEWSIZE), (VECPTR)->bufsize, sizeof((VECPTR)->data), (ALLOC));\
-		if (newptr != NULL) {\
-			(VECPTR)->data = newptr;\
-			(VECPTR)->bufsize = (NEWSIZE);\
+// Reserves space for exactly NEWSIZE elements in VEC.
+// If NEWSIZE < rudp_vector_capacity(VEC), nothing happens.
+#define rudp_vector_reserve_exact(VEC, NEWSIZE, ALLOC) ({\
+	int RUDP_VECTOR_RESERVE_EXACT_RETVAL_INTERNAL_ = 0;\
+	if ((NEWSIZE) > (VEC).bufsize) {\
+		typeof((VEC).data) RUDP_VECTOR_RESERVE_EXACT_NEWPTR_INTERNAL = rudp_allocator_reallocarray((VEC).data, (NEWSIZE), (VEC).bufsize, sizeof((VEC).data), (ALLOC));\
+		if (RUDP_VECTOR_RESERVE_EXACT_NEWPTR_INTERNAL != NULL) {\
+			(VEC).data = RUDP_VECTOR_RESERVE_EXACT_NEWPTR_INTERNAL;\
+			(VEC).bufsize = (NEWSIZE);\
 		}\
 		else {\
-			retval = -1;\
+			RUDP_VECTOR_RESERVE_EXACT_RETVAL_INTERNAL_ = -1;\
 		}\
 	}\
-	retval;\
+	RUDP_VECTOR_RESERVE_EXACT_RETVAL_INTERNAL_;\
 })
 
--- src/utils/tests/test_vector.c
+++ src/utils/tests/test_vector.c
@@ -1,27 +1,26 @@
 #include <rudp/utils/vector.h>
 #include <assert.h>
 
-// YAYAYAY
 int main(void)
 {
 	int nums[] = {1, 11, 111, 1111, 11111, 111111, 23, 45, 67, 89, 10, 11, 12, 13};
 	RUDP_VECTOR_TYPE(int) vec;
-	rudp_vector_new(&vec, NULL);
+	rudp_vector_new(vec, NULL);
 	for (int i = 0; i < 14; ++i) {
-		assert(rudp_vector_push(&vec, nums + i, NULL) == 0);
+		assert(rudp_vector_push(vec, nums[i], NULL) == 0);
 	}
-	assert(vec.nel == rudp_vector_size(&vec) && rudp_vector_size(&vec) == 14);
-	assert(vec.bufsize == rudp_vector_capacity(&vec) && rudp_vector_capacity(&vec) == 16);
+	assert(vec.nel == rudp_vector_size(vec) && rudp_vector_size(vec) == 14);
+	assert(vec.bufsize == rudp_vector_capacity(vec) && rudp_vector_capacity(vec) == 16);
 	for (int i = 0; i < 14; ++i) {
-		assert(*rudp_vector_at(&vec, i) == nums[i]);
+		assert(*rudp_vector_at(vec, i) == nums[i]);
 	}
 	for (int i = 0; i < 7; ++i) {
-		rudp_vector_pop(&vec);
+		rudp_vector_pop(vec);
 	}
-	assert(rudp_vector_size(&vec) == 7);
-	assert(rudp_vector_capacity(&vec) == 16);
-	rudp_vector_erase(&vec);
-	assert(rudp_vector_size(&vec) == 0);
-	assert(rudp_vector_capacity(&vec) == 16);
-	rudp_vector_delete(&vec, NULL);
+	assert(rudp_vector_size(vec) == 7);
+	assert(rudp_vector_capacity(vec) == 16);
+	rudp_vector_erase(vec);
+	assert(rudp_vector_size(vec) == 0);
+	assert(rudp_vector_capacity(vec) == 16);
+	rudp_vector_delete(vec, NULL);
 }
